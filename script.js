ac64c9fef82a9ba74cf466083bea7e202b0fbdbb
srujana1233 <kakunurisrujana@gmail.com>
2021-08-23 13:48:02 +0530

    changes done

---
+++ script.js
@@ -0,0 +1,40 @@
+const container = document.querySelector('.screen-container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+
+const movieSelect = document.getElementById('movie');
+var  ticketPrice = +movieSelect.value;
+
+//functions
+const updateSelectedCount = function () {
+  const selectedSeats = document.querySelectorAll('.row.seat.selected');
+  //console.log(selectedSeats);
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+};
+
+//event listners
+
+//movie change event listner
+movieSelect.addEventListener('change',(e)=> {
+  ticketPrice = +e.target.value;
+  console.log(ticketPrice);
+  updateSelectedCount();
+
+})
+
+//seat selector event listner
+container.addEventListener('click',(e) => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+) {
+    e.target.classList.toggle('selected');
+   
+}
+});
+//on refresh
+updateSelectedCount();
