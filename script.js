d95b7f8a8fcee73314749db3eea84f8a29373b98
srujana1233 <kakunurisrujana@gmail.com>
2021-08-25 20:47:53 +0530

    changes updated

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const container = document.querySelector('.screen-container');
-const seat = document.querySelectorAll('.row .seat:not(.occupied)');
-
+const screenContainer = document.querySelector('.screen-container');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');// It takes all the seats which are not with class occupied. 
 const count = document.getElementById('count');
 const total = document.getElementById('total');
-
 const movieSelect = document.getElementById('movie');
-var  ticketPrice = +movieSelect.value;
+let ticketPrice = +movieSelect.value;// + because to change ticket price from string to int.
 
-//functions
-const updateSelectedCount = function () {
-  const selectedSeats = document.querySelectorAll('.row.seat.selected');
+//Functions
+
+const updateSelectedCount = function(){
+  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+  // console.log(selectedSeats);
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
-};
 
-//event listners
+}
 
-//movie change event listner
-movieSelect.addEventListener('change',(e)=> {
+
+//Event listeners
+
+//1. seat selection event listener
+screenContainer.addEventListener('click', (e) =>{
+  if(e.target.classList.contains('seat') && (!e.target.classList.contains('occupied')) ){
+    e.target.classList.toggle('selected');
+    updateSelectedCount();
+  }
+})
+
+//2. movie select event listener
+movieSelect.addEventListener('change',(e) =>{
   ticketPrice = +e.target.value;
   console.log(ticketPrice);
   updateSelectedCount();
-
 })
 
-//seat selector event listner
-container.addEventListener('click',(e) => {
-  if (
-    (e.target.classList.contains('seat') &&
-    (!e.target.classList.contains('occupied')))
-) {
-    e.target.classList.toggle('selected');
-    updateSelectedCount();
-
-   
-}
-});
 //on refresh
 updateSelectedCount();
